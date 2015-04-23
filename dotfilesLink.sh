8b8944271ecbf2f84b46b7fd4ee1b0dde4258fa7
myasu26 <hotshot26jp@gmail.com>
2015-04-24 08:41:40 +0900

    .zshrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sfn ~/dotfiles/vimfiles ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
